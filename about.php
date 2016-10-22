c0b0c395c8ec225b0e897703951e5eb62d59ebf0
Shetay <shety19@gmail.com>
2016-10-23 00:38:51 +0200

    Added contact and the quote

---
+++ about.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+include("header.php");
+
+?>
+
+    <center><p>"BLOC was a failure." - Evan Hoovestool</p>
+<br /><br />
+an actually working game by Shetay and other cool dudes on <a href="https://github.com/Shetay/Realpolitik">github</a>;
+
+
+<br /><br />
+
+contact me at github, discord or shety19@gmail.com 
+
+<?php
+
+include("footer.php");
+?>
